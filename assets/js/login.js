f393fd637d1fcdb855c212376d1d7a30f1129cd0
fufuhaole <839379837@qq.com>
2020-09-09 18:39:10 +0800

    登录注册页面完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,10 +1,28 @@
 $(function () {
     $('.link-login').on('click', function () {
-        $('.regBox').hide()
-        $('.loginBox').show()
+        $('.loginBox').hide()
+        $('.regBox').show()
     })
     $('.link-reg').on('click', function () {
-        $('.regBox').show()
-        $('.loginBox').hide()
+        $('.loginBox').show()
+        $('.regBox').hide()
     })
+
+    // 用户名密码正则验证
+    var form = layui.form
+    form.verify({
+        pass: [
+            /^[\S]{6,12}$/
+            ,'密码必须6到12位，且不能出现空格'
+          ] ,
+        repass: function (value) {
+            var password = $('.regBox [name=password]').val()
+            if (value !== password) {
+                return "两次密码不一致"
+            }    
+        }
+    })
+
+
 })
+
